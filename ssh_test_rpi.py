28f7630e58644ba9e771915013779d325f1314b0
volodymyr <vsp.dmmi@gmail.com>
2024-11-19 13:31:02 -0500

    v0.1

--- ssh_test_rpi.py
+++ ssh_test_rpi.py
@@ -1,6 +1,7 @@
 import paramiko
 import time
 import logging
+from private import *
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
@@ -8,11 +9,11 @@ logger = logging.getLogger()
 
 def get_user_input():
     """Запрашивает у пользователя данные для подключения."""
-    host = input("Введите IP-адрес Raspberry Pi: ").strip()
-    username = input("Введите имя пользователя (по умолчанию 'pi'): ").strip() or "pi"
-    password = input("Введите пароль: ").strip()
+    host = input("Введите IP-адрес Raspberry Pi: ").strip() or "192.168.8.209"
+    username = input("Введите имя пользователя (по умолчанию 'pi'): ").strip() or LOGIN
+    password = input("Введите пароль: ").strip() or PASSWD
     interval = input("Введите интервал проверки в секундах (по умолчанию 300): ").strip()
-    interval = int(interval) if interval.isdigit() else 300
+    interval = int(interval) if interval.isdigit() else 3
     return host, username, password, interval
 
 def check_raspberry_pi(host, username, password):
@@ -26,10 +27,21 @@ def check_raspberry_pi(host, username, password):
         ssh.connect(host, username=username, password=password, timeout=10)
         logger.info("Raspberry Pi доступен.")
         
-        # Выполняем команду (например, проверяем uptime)
+       # Проверяем uptime
         stdin, stdout, stderr = ssh.exec_command('uptime')
         uptime = stdout.read().decode().strip()
-        logger.info(f"Uptime: {uptime}\n")
+        logger.info(f">>>\nUptime:\n{uptime}\n")
+
+        # Проверяем память
+        stdin, stdout, stderr = ssh.exec_command('free -h')
+        memory_status = stdout.read().decode().strip()
+        logger.info(f"Memory Status:\n{memory_status}\n")
+
+        # Проверяем загрузку процессора
+        stdin, stdout, stderr = ssh.exec_command('top -bn1 | grep "Cpu(s)"')
+        cpu_usage = stdout.read().decode().strip()
+        logger.info(f"CPU Usage:\n{cpu_usage}\n")
+
         
         ssh.close()
     except Exception as e:
